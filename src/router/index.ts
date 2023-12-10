e125051ba587dc50a3a5a7535bcf881812d015f2
William Arvidsson <william.arvidsson@gmail.com>
2023-12-10 20:35:13 +0100

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import PrivacyView from '../views/PrivacyView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/privacy',
+      name: 'privacy',
+      component: PrivacyView 
+    }
+  ]
+})
+
+export default router
---
+++ src/views/PrivacyView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="privacy">
+    <h1>This is a privacy page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .privacy {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
